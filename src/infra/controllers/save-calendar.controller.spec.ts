40c290225db958a1ee8bdbe3346dd624d05877e1
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-01 09:19:07 -0300

    refactor: move production code to its own file

--- src/infra/controllers/save-calendar.controller.spec.ts
+++ src/infra/controllers/save-calendar.controller.spec.ts
@@ -1,33 +1,8 @@
 import { GetCalendarByNameUseCaseInterface } from '@/application/interfaces/get-calendar-by-name-usecase.interface'
 import { SaveCalendarUseCaseInterface } from '@/application/interfaces/save-calendar-usecase.interface'
 import { MissingParamError, ResourceConflictError } from '@/shared/errors'
-import { badRequest, conflict, serverError, success } from '@/shared/helpers/http'
-import { HttpResponse } from '@/shared/types/http'
-
-export class SaveCalendarController {
-  constructor (
-    private readonly getCalendarByNameUseCase: GetCalendarByNameUseCaseInterface,
-    private readonly saveCalendarUseCase: SaveCalendarUseCaseInterface
-  ) {}
-
-  async execute (name: string): Promise<HttpResponse> {
-    try {
-      if (!name) {
-        return badRequest(new MissingParamError('name'))
-      }
-
-      const calendarExists = await this.getCalendarByNameUseCase.execute(name)
-      if (calendarExists) {
-        return conflict(new ResourceConflictError('This name already exists'))
-      }
-
-      const newCalendar = await this.saveCalendarUseCase.execute({ name })
-      return success(201, newCalendar)
-    } catch (error) {
-      return serverError(error)
-    }
-  }
-}
+import { badRequest, conflict, serverError } from '@/shared/helpers/http'
+import { SaveCalendarController } from './save-calendar.controller'
 
 describe('SaveCalendarController', () => {
   let input: any
