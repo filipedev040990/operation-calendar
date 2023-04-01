18f56d47998452f32ccb191be633cb98aa92feb8
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-01 08:00:18 -0300

    feat: ensure GetCalendarByNameUseCase return a calendar

--- src/application/usecases/get-calendar-by-name.spec.ts
+++ src/application/usecases/get-calendar-by-name.spec.ts
@@ -1,25 +1,41 @@
 import { GetCalendarByNameRepository } from '@/application/interfaces'
 import { CalendarEntity } from '@/domain/entities/calendar.entity'
 
-export class GetCalendarByName {
+export class GetCalendarByNameUseCase {
   constructor (private readonly calendarRepository: GetCalendarByNameRepository) {}
   async execute (name: string): Promise<CalendarEntity> | null {
-    await this.calendarRepository.getByName(name)
-    return null
+    return await this.calendarRepository.getByName(name)
   }
 }
 
-describe('GetCalendarByName', () => {
-  const name = 'Test'
+describe('GetCalendarByNameUseCase', () => {
   const calendarRepository: jest.Mocked<GetCalendarByNameRepository> = {
-    getByName: jest.fn().mockResolvedValue(name)
+    getByName: jest.fn()
   }
   test('should call CalendarRepository.getByName once and with correct name', async () => {
-    const sut = new GetCalendarByName(calendarRepository)
+    const sut = new GetCalendarByNameUseCase(calendarRepository)
 
-    await sut.execute(name)
+    await sut.execute('Test')
 
     expect(calendarRepository.getByName).toHaveBeenCalledTimes(1)
-    expect(calendarRepository.getByName).toHaveBeenCalledWith(name)
+    expect(calendarRepository.getByName).toHaveBeenCalledWith('Test')
+  })
+
+  test('should return a calendar', async () => {
+    calendarRepository.getByName.mockResolvedValueOnce({
+      id: '123456789',
+      name: 'Test',
+      created_at: new Date('2023-01-01')
+    })
+
+    const sut = new GetCalendarByNameUseCase(calendarRepository)
+
+    const calendar = await sut.execute('Test')
+
+    expect(calendar).toEqual({
+      id: '123456789',
+      name: 'Test',
+      created_at: new Date('2023-01-01')
+    })
   })
 })
