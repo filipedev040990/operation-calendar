a2048f4ab7cc120a8163a74c1de4071521d66c4b
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-01 08:03:58 -0300

    refactor: isolate SUT creation

--- src/application/usecases/get-calendar-by-name.spec.ts
+++ src/application/usecases/get-calendar-by-name.spec.ts
@@ -13,9 +13,13 @@ describe('GetCalendarByNameUseCase', () => {
   const calendarRepository: jest.Mocked<GetCalendarByNameRepository> = {
     getByName: jest.fn()
   }
-  test('should call CalendarRepository.getByName once and with correct name', async () => {
-    const sut = new GetCalendarByNameUseCase(calendarRepository)
 
+  let sut: GetCalendarByNameUseCase
+  beforeAll(() => {
+    sut = new GetCalendarByNameUseCase(calendarRepository)
+  })
+
+  test('should call CalendarRepository.getByName once and with correct name', async () => {
     await sut.execute('Test')
 
     expect(calendarRepository.getByName).toHaveBeenCalledTimes(1)
@@ -26,23 +30,19 @@ describe('GetCalendarByNameUseCase', () => {
     calendarRepository.getByName.mockResolvedValueOnce({
       id: '123456789',
       name: 'Test',
-      created_at: new Date('2023-01-01')
+      created_at: new Date('2023-01-01 15:11:25')
     })
-
-    const sut = new GetCalendarByNameUseCase(calendarRepository)
 
     const calendar = await sut.execute('Test')
 
     expect(calendar).toEqual({
       id: '123456789',
       name: 'Test',
-      created_at: new Date('2023-01-01')
+      created_at: new Date('2023-01-01 15:11:25')
     })
   })
 
   test('should return null if CalendarRepository.getByName returns null', async () => {
-    const sut = new GetCalendarByNameUseCase(calendarRepository)
-
     const calendar = await sut.execute('Test')
 
     expect(calendar).toBeNull()
