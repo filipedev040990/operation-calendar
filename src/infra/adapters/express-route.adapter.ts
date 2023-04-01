524830d8c26d0b31bb1db174b5d0e6df74f57db2
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-01 10:03:04 -0300

    feat: implements express router adapter

---
+++ src/infra/adapters/express-route.adapter.ts
@@ -0,0 +1,10 @@
+import { HttpResponse } from '@/shared/types/http'
+import { Request, Response } from 'express'
+
+export const expressRouterAdapter = (controller: any) => {
+  return async (req: Request, res: Response) => {
+    const response: HttpResponse = await controller.execute(req.body)
+    const bodyResponse = response.statusCode === 500 ? { error: response.body.error } : response.body
+    res.status(response.statusCode).json(bodyResponse)
+  }
+}
