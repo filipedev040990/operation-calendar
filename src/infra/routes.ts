1c0bdae0f2d41456c5dcf4865deb4dd039775978
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-02 09:00:43 -0300

    feat: add get calendar by name route

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { expressRouterAdapter } from './adapters/express-route.adapter'
 import { makeSaveCalendarControllerFactory } from './factories/controllers/save-calendar-controller.factory'
 import { makeListAllCalendarsControllerFactory } from './factories/controllers/list-all-calendar-controller.factory'
+import { makeListCalendarByNameControllerFactory } from './factories/controllers/list-calendar-by-name-controller.factory'
 
 const router = Router()
 
 router.post('/calendar', expressRouterAdapter(makeSaveCalendarControllerFactory()))
 router.get('/calendar', expressRouterAdapter(makeListAllCalendarsControllerFactory()))
+router.get('/calendar/:name', expressRouterAdapter(makeListCalendarByNameControllerFactory()))
 
 export { router }
