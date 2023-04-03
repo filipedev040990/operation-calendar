7212d995a954e0fc0dfe52a2a5fadd2c543d1133
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-03 16:53:52 -0300

    feat: ensure UpdateCalendarController call UpdateCalendarUseCase once and with correct values

--- src/infra/controllers/calendars/update-calendar.controller.spec.ts
+++ src/infra/controllers/calendars/update-calendar.controller.spec.ts
@@ -3,14 +3,25 @@ import { badRequest, conflict } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
 import { GetCalendarByNameUseCaseInterface } from '@/application/interfaces/get-calendar-by-name-usecase.interface'
+import { UpdateCalendarUseCaseInterface } from '@/application/interfaces/update-calendar-usecase.interface'
 
 export class UpdateCalendarController implements ControllerInterface {
-  constructor (private readonly getCalendarByNameUseCase: GetCalendarByNameUseCaseInterface) {}
+  constructor (
+    private readonly getCalendarByNameUseCase: GetCalendarByNameUseCaseInterface,
+    private readonly updateCalendarUseCase: UpdateCalendarUseCaseInterface
+  ) {}
+
   async execute (input: HttpRequest): Promise<HttpResponse> {
-    const error = this.validateInput(input)
+    const error = await this.validateInput(input)
     if (error) {
-      return await error
+      return error
     }
+
+    const updateInput = {
+      id: input.params.id,
+      name: input.body.name
+    }
+    await this.updateCalendarUseCase.execute(updateInput)
 
     return null
   }
@@ -35,15 +46,20 @@ const getCalendarByNameUseCase: jest.Mocked<GetCalendarByNameUseCaseInterface> =
   execute: jest.fn()
 }
 
+const updateCalendarUseCase: jest.Mocked<UpdateCalendarUseCaseInterface> = {
+  execute: jest.fn()
+}
+
 describe('UpdateCalendarController', () => {
   let sut: UpdateCalendarController
   let input: HttpRequest
 
   beforeAll(() => {
-    sut = new UpdateCalendarController(getCalendarByNameUseCase)
+    sut = new UpdateCalendarController(getCalendarByNameUseCase, updateCalendarUseCase)
   })
 
   beforeEach(() => {
+    jest.clearAllMocks()
     input = {
       params: {
         id: '123456789'
@@ -84,5 +100,12 @@ describe('UpdateCalendarController', () => {
     const response = await sut.execute(input)
 
     expect(response).toEqual(conflict(new ResourceConflictError('This name already exists')))
+  })
+
+  test('should call UpdateCalendarUseCase once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(updateCalendarUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(updateCalendarUseCase.execute).toHaveBeenCalledWith({ id: input.params.id, name: input.body.name })
   })
 })
