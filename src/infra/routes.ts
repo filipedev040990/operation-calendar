c1f23b17064805aadc32554edb72200a98ff4fd3
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-03 17:09:51 -0300

    feat: add update calendar route

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -3,11 +3,13 @@ import { expressRouterAdapter } from './adapters/express-route.adapter'
 import { makeSaveCalendarControllerFactory } from './factories/controllers/save-calendar-controller.factory'
 import { makeListAllCalendarsControllerFactory } from './factories/controllers/list-all-calendar-controller.factory'
 import { makeListCalendarByNameControllerFactory } from './factories/controllers/list-calendar-by-name-controller.factory'
+import { makeUpdateCalendarControllerFactory } from './factories/controllers/update-calendar-controller.factory'
 
 const router = Router()
 
 router.post('/calendar', expressRouterAdapter(makeSaveCalendarControllerFactory()))
 router.get('/calendar', expressRouterAdapter(makeListAllCalendarsControllerFactory()))
 router.get('/calendar/:name', expressRouterAdapter(makeListCalendarByNameControllerFactory()))
+router.put('/calendar/:id', expressRouterAdapter(makeUpdateCalendarControllerFactory()))
 
 export { router }
