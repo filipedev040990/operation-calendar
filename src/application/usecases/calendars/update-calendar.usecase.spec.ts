89721efadda219957d0fa28f7d8a17aa265f61c4
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-03 12:10:53 -0300

    feat: ensure UpdateCalendarUseCase return a calendar updated

--- src/application/usecases/calendars/update-calendar.usecase.spec.ts
+++ src/application/usecases/calendars/update-calendar.usecase.spec.ts
@@ -1,10 +1,11 @@
 import { UpdateCalendarRepositoryInterface } from '@/application/interfaces'
 import { UpdateCalendarUseCaseInterface } from '@/application/interfaces/update-calendar-usecase.interface'
+import MockDate from 'mockdate'
 
 const calendarRepository: jest.Mocked<UpdateCalendarRepositoryInterface> = {
   update: jest.fn().mockResolvedValue({
     id: '123456789',
-    name: 'Any Name',
+    name: 'Updated Name',
     created_at: new Date('2022-12-31 16:15:12')
   })
 }
@@ -17,18 +18,35 @@ const input: UpdateCalendarUseCaseInterface.Input = {
 export class UpdateCalendarUseCase implements UpdateCalendarUseCaseInterface {
   constructor (private readonly calendarRepository: UpdateCalendarRepositoryInterface) {}
   async execute (input: UpdateCalendarUseCaseInterface.Input): Promise<UpdateCalendarUseCaseInterface.Output> {
-    await this.calendarRepository.update(input)
-    return null
+    const calendarUpdated = await this.calendarRepository.update({
+      id: input.id,
+      name: input.name
+    })
+    return calendarUpdated
   }
 }
 
 describe('UpdateCalendarUseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('should call CalendarRepository.update once and with correct values', async () => {
     const sut = new UpdateCalendarUseCase(calendarRepository)
 
     await sut.execute(input)
 
     expect(calendarRepository.update).toHaveBeenCalledTimes(1)
-    expect(calendarRepository.update).toHaveBeenCalledWith(input)
+    expect(calendarRepository.update).toHaveBeenCalledWith({ id: '123456789', name: 'Updated Name' })
+  })
+
+  test('should return a calendar updated', async () => {
+    const sut = new UpdateCalendarUseCase(calendarRepository)
+
+    const response = await sut.execute(input)
+
+    expect(response).toEqual({ id: '123456789', name: 'Updated Name', created_at: new Date('2022-12-31 16:15:12') })
   })
 })
