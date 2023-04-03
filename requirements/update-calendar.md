069c0b729c3e3d909fbc3f8a183e7c874c4feb33
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-03 12:27:18 -0300

    refactor: isolate SUT creation

--- requirements/update-calendar.md
+++ requirements/update-calendar.md
@@ -4,7 +4,7 @@
 
 1 - ✅ Deve retornar **400** se o id do calendário não for fornecido
 2 - ⛔ Deve retornar **400** se o nome do calendário não for fornecido
-3 - ⛔ Deve retornar **400** se já existir um calendário com o nome fornecido
+3 - ⛔ Deve retornar **409** se já existir um calendário com o nome fornecido
 4 - ⛔ Deve retornar **200** com os dados do calendário atualizado
 
 
--- src/infra/controllers/calendars/update-calendar.controller.spec.ts
+++ src/infra/controllers/calendars/update-calendar.controller.spec.ts
@@ -3,15 +3,6 @@ import { badRequest } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
 
-const input = {
-  params: {
-    id: '123456789'
-  },
-  body: {
-    name: 'Updated Name'
-  }
-}
-
 export class UpdateCalendarController implements ControllerInterface {
   async execute (input: HttpRequest): Promise<HttpResponse> {
     if (!input.params?.id) {
@@ -22,9 +13,24 @@ export class UpdateCalendarController implements ControllerInterface {
 }
 
 describe('UpdateCalendarController', () => {
-  test('should return 400 if calendar id is not provided', async () => {
-    const sut = new UpdateCalendarController()
+  let sut: UpdateCalendarController
+  let input: HttpRequest
 
+  beforeAll(() => {
+    sut = new UpdateCalendarController()
+  })
+
+  beforeEach(() => {
+    input = {
+      params: {
+        id: '123456789'
+      },
+      body: {
+        name: 'Updated Name'
+      }
+    }
+  })
+  test('should return 400 if calendar id is not provided', async () => {
     input.params.id = null
     const response = await sut.execute(input)
 
