7d06c0e7925e80e31a3d92e58ce449b9e969c1b8
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-04 16:34:18 -0300

    feat: ensure DeleteCalendarController return 400 if id is not provided

---
+++ src/infra/controllers/calendars/delete-calendar.controller.spec.ts
@@ -0,0 +1,30 @@
+import { ControllerInterface } from '@/infra/interfaces/controller.interface'
+import { MissingParamError } from '@/shared/errors'
+import { badRequest } from '@/shared/helpers/http'
+import { HttpRequest, HttpResponse } from '@/shared/types/http'
+
+export class DeleteCalendarController implements ControllerInterface {
+  async execute (input: HttpRequest): Promise<HttpResponse> {
+    if (!input.params?.id) {
+      return badRequest(new MissingParamError('id'))
+    }
+    return null
+  }
+}
+
+const input: HttpRequest = {
+  params: {
+    id: 'anyId'
+  }
+}
+
+describe('DeleteCalendarController', () => {
+  test('should return 400 if id is not provided', async () => {
+    input.params.id = null
+    const sut = new DeleteCalendarController()
+
+    const response = await sut.execute(input)
+
+    expect(response).toEqual(badRequest(new MissingParamError('id')))
+  })
+})
