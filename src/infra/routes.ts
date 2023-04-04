d37bc390bc0734481c1bd9bf171011bd3c6d2a6f
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-04 17:10:30 -0300

    feat: add delete calendar route

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -4,6 +4,7 @@ import { makeSaveCalendarControllerFactory } from './factories/controllers/save-
 import { makeListAllCalendarsControllerFactory } from './factories/controllers/list-all-calendar-controller.factory'
 import { makeGetCalendarByNameControllerFactory } from './factories/controllers/get-calendar-by-name-controller.factory'
 import { makeUpdateCalendarControllerFactory } from './factories/controllers/update-calendar-controller.factory'
+import { makeDeleteCalendarControllerFactory } from './factories/controllers/delete-calendar-controller.factory'
 
 const router = Router()
 
@@ -11,5 +12,6 @@ router.post('/calendar', expressRouterAdapter(makeSaveCalendarControllerFactory(
 router.get('/calendar', expressRouterAdapter(makeListAllCalendarsControllerFactory()))
 router.get('/calendar/:name', expressRouterAdapter(makeGetCalendarByNameControllerFactory()))
 router.put('/calendar/:id', expressRouterAdapter(makeUpdateCalendarControllerFactory()))
+router.delete('/calendar/:id', expressRouterAdapter(makeDeleteCalendarControllerFactory()))
 
 export { router }
