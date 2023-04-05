b2d9a56dcc4389b567e37a987da5b3c8c9348aad
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-05 17:08:12 -0300

    feat: ensure GetEventByNameUseCase returns a calendar if exists

--- src/application/usecases/event/get-event-by-name.usecase.spec.ts
+++ src/application/usecases/event/get-event-by-name.usecase.spec.ts
@@ -16,8 +16,8 @@ const enventRepository: jest.Mocked<GetEventByNameRepositoryInterface> = {
 export class GetEventByNameUseCase implements GetEventByNameUseCaseInterface {
   constructor (private readonly enventRepository: GetEventByNameRepositoryInterface) {}
   async execute (name: string): Promise<EventEntity> {
-    await this.enventRepository.getByName(name)
-    return null
+    const calendar = await this.enventRepository.getByName(name)
+    return calendar ?? null
   }
 }
 
@@ -25,9 +25,24 @@ describe('GetEventByNameUseCase', () => {
   test('should call EventRepository once and with correct name', async () => {
     const sut = new GetEventByNameUseCase(enventRepository)
 
-    await sut.execute('AnyName')
+    await sut.execute('Any Event')
 
     expect(enventRepository.getByName).toHaveBeenCalledTimes(1)
-    expect(enventRepository.getByName).toHaveBeenCalledWith('AnyName')
+    expect(enventRepository.getByName).toHaveBeenCalledWith('Any Event')
+  })
+
+  test('should return a calendar if exists', async () => {
+    const sut = new GetEventByNameUseCase(enventRepository)
+
+    const response = await sut.execute('Any Event')
+
+    expect(response).toEqual({
+      id: 'anyEventId',
+      calendar_id: 'anyCalendarId',
+      name: 'Any Event',
+      category: 'NORMAL',
+      start_date: new Date('2023-01-01 13:00:00'),
+      end_date: new Date('2023-01-01 13:00:00')
+    })
   })
 })
