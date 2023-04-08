3b187815295c961e3d5eb75ef362997c71b2e4c6
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-08 18:26:33 -0300

    feat: ensure GetAllEventsController call GetAllEventsUseCase once

---
+++ src/infra/controllers/events/get-all-events.controller.spec.ts
@@ -0,0 +1,34 @@
+import { GetAllEventsUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
+import { ControllerInterface } from '@/infra/interfaces/controller.interface'
+import { HttpResponse } from '@/shared/types/http'
+
+export class GetAllEventsController implements ControllerInterface {
+  constructor (private readonly getAllEventsUseCase: GetAllEventsUseCaseInterface) {}
+  async execute (): Promise<HttpResponse> {
+    await this.getAllEventsUseCase.execute()
+    return null
+  }
+}
+
+const fakeEvents = [{
+  event_id: 'anyEventId',
+  event_name: 'anyEventName',
+  start_date: new Date('2023-01-01 15:20:15'),
+  end_date: new Date('2023-01-01 15:20:15'),
+  calendar_id: 'anyCalendarId',
+  calendar_name: 'anyCalendarName'
+}]
+
+const getAllEventsUseCase: jest.Mocked<GetAllEventsUseCaseInterface> = {
+  execute: jest.fn().mockResolvedValue(fakeEvents)
+}
+
+describe('GetAllEventsController', () => {
+  test('should call GetAllEventsUseCase once', async () => {
+    const sut = new GetAllEventsController(getAllEventsUseCase)
+
+    await sut.execute()
+
+    expect(getAllEventsUseCase.execute).toHaveBeenCalledTimes(1)
+  })
+})
