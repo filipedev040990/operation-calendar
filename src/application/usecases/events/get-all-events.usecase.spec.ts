44d17108e9a380b96e6369c11fadc7c3f1528560
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-08 18:18:26 -0300

    feat: ensure GetAllEventsUseCase returns all events

--- src/application/usecases/events/get-all-events.usecase.spec.ts
+++ src/application/usecases/events/get-all-events.usecase.spec.ts
@@ -4,20 +4,22 @@ import { GetAllEventsUseCaseInterface } from '@/application/interfaces/event-use
 export class GetAllEventsUseCase implements GetAllEventsUseCaseInterface {
   constructor (private readonly eventRepository: GetAllEventsRepositoryInterface) {}
   async execute (): Promise<GetAllEventsUseCaseInterface.Output> {
-    await this.eventRepository.getAll()
-    return null
+    const events = await this.eventRepository.getAll()
+    return events ?? null
   }
 }
 
+const fakeEvents = [{
+  event_id: 'anyEventId',
+  event_name: 'anyEventName',
+  start_date: new Date('2023-01-01 15:20:15'),
+  end_date: new Date('2023-01-01 15:20:15'),
+  calendar_id: 'anyCalendarId',
+  calendar_name: 'anyCalendarName'
+}]
+
 const eventRepository: jest.Mocked<GetAllEventsRepositoryInterface> = {
-  getAll: jest.fn().mockResolvedValue({
-    event_id: 'anyEventId',
-    event_name: 'anyEventName',
-    start_date: new Date('2023-01-01 15:20:15'),
-    end_date: new Date('2023-01-01 15:20:15'),
-    calendar_id: 'anyCalendarId',
-    calendar_name: 'anyCalendarName'
-  })
+  getAll: jest.fn().mockResolvedValue(fakeEvents)
 }
 
 describe('GetAllEventsUseCase', () => {
@@ -31,5 +33,11 @@ describe('GetAllEventsUseCase', () => {
     await sut.execute()
 
     expect(eventRepository.getAll).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return all events', async () => {
+    const response = await sut.execute()
+
+    expect(response).toEqual(fakeEvents)
   })
 })
