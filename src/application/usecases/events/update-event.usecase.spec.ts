43a6cddee22f8cc5960a5f5818311a8488a0044d
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-11 16:23:31 -0300

    feat: ensure UpdateEventUseCase  set start_date to end_date if null

--- src/application/usecases/events/update-event.usecase.spec.ts
+++ src/application/usecases/events/update-event.usecase.spec.ts
@@ -38,6 +38,11 @@ describe('UpdateEventUseCase', () => {
   beforeAll(() => {
     sut = new UpdateEventUseCase(eventRepository)
   })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('should call EventRepository.update once and with correct values', async () => {
     await sut.execute(input)
 
@@ -49,6 +54,22 @@ describe('UpdateEventUseCase', () => {
       name: 'anyName',
       start_date: new Date('2023-01-01'),
       end_date: new Date('2023-01-03')
+    })
+  })
+
+  test('should set start_date to end_date if null', async () => {
+    input.end_date = null
+
+    await sut.execute(input)
+
+    expect(eventRepository.update).toHaveBeenCalledTimes(1)
+    expect(eventRepository.update).toHaveBeenCalledWith({
+      id: 'anyId',
+      calendar_id: 'anyCalendarId',
+      category: 'NORMAL',
+      name: 'anyName',
+      start_date: new Date('2023-01-01'),
+      end_date: new Date('2023-01-01')
     })
   })
 })
