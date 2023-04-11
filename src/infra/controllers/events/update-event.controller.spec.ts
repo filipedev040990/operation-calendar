8df9a31757ac55b9318e8141c8d80e72b58798d9
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-11 18:42:08 -0300

    feat: done all tests update controller

--- src/infra/controllers/events/update-event.controller.spec.ts
+++ src/infra/controllers/events/update-event.controller.spec.ts
@@ -1,43 +1,57 @@
 import { GetCalendarByIdUseCaseInterface } from '@/application/interfaces'
-import { GetEventByNameUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
+import { GetEventByNameUseCaseInterface, UpdateEventUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
 import { InvalidParamError, MissingParamError, ResourceConflictError } from '@/shared/errors'
-import { badRequest, conflict } from '@/shared/helpers/http'
+import { badRequest, conflict, serverError, success } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 import MockDate from 'mockdate'
 
 export class UpdateEventController implements ControllerInterface {
   constructor (
     private readonly getCalendarByIdUseCase: GetCalendarByIdUseCaseInterface,
-    private readonly getEventByName: GetEventByNameUseCaseInterface
+    private readonly getEventByName: GetEventByNameUseCaseInterface,
+    private readonly updateEventUseCase: UpdateEventUseCaseInterface
   ) {}
 
   async execute (input: HttpRequest): Promise<HttpResponse> {
-    const missingParam = this.requiredParamsValidator(input)
-    if (missingParam) {
-      return badRequest(new MissingParamError(missingParam))
-    }
+    try {
+      const missingParam = this.requiredParamsValidator(input)
+      if (missingParam) {
+        return badRequest(new MissingParamError(missingParam))
+      }
 
-    const invalidCategory = this.categoryValidator(input.body.category)
-    if (invalidCategory) {
-      return badRequest(new InvalidParamError('category'))
-    }
+      const invalidCategory = this.categoryValidator(input.body.category)
+      if (invalidCategory) {
+        return badRequest(new InvalidParamError('category'))
+      }
 
-    const calendar = await this.getCalendarByIdUseCase.execute(input.body.calendar_id)
-    if (!calendar) {
-      return badRequest(new InvalidParamError('calendar_id'))
-    }
+      const calendar = await this.getCalendarByIdUseCase.execute(input.body.calendar_id)
+      if (!calendar) {
+        return badRequest(new InvalidParamError('calendar_id'))
+      }
 
-    const event = await this.getEventByName.execute(input.body.name)
-    if (event && event.id !== input.params.id) {
-      return conflict(new ResourceConflictError('This event already exists'))
-    }
+      const event = await this.getEventByName.execute(input.body.name)
+      if (event && event.id !== input.params.id) {
+        return conflict(new ResourceConflictError('This event already exists'))
+      }
 
-    const invalidEndDate = this.endDateValidator(input)
-    if (invalidEndDate) {
-      return badRequest(new InvalidParamError('end_date'))
+      const invalidEndDate = this.endDateValidator(input)
+      if (invalidEndDate) {
+        return badRequest(new InvalidParamError('end_date'))
+      }
+
+      const newEvent = await this.updateEventUseCase.execute({
+        id: input.params.id,
+        calendar_id: input.body.calendar_id,
+        name: input.body.name,
+        category: input.body.category,
+        start_date: input.body.start_date,
+        end_date: input.body.end_date ?? input.body.start_date
+      })
+      return success(201, newEvent)
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 
   private requiredParamsValidator (input: HttpRequest): string | void {
@@ -77,13 +91,24 @@ const getEventByName: jest.Mocked<GetEventByNameUseCaseInterface> = {
   execute: jest.fn().mockResolvedValue(null)
 }
 
+const updateEventUseCase: jest.Mocked<UpdateEventUseCaseInterface> = {
+  execute: jest.fn().mockResolvedValue({
+    id: 'anyId',
+    calendar_id: 'anyCalendarId',
+    category: 'NORMAL',
+    name: 'anyName',
+    start_date: new Date('2023-01-01'),
+    end_date: new Date('2023-01-03')
+  })
+}
+
 describe('UpdateEventController', () => {
   let sut: UpdateEventController
   let input: HttpRequest
 
   beforeAll(() => {
     MockDate.set(new Date())
-    sut = new UpdateEventController(getCalendarByIdUseCase, getEventByName)
+    sut = new UpdateEventController(getCalendarByIdUseCase, getEventByName, updateEventUseCase)
   })
 
   beforeEach(() => {
@@ -168,5 +193,46 @@ describe('UpdateEventController', () => {
     const response = await sut.execute(input)
 
     expect(response).toEqual(badRequest(new InvalidParamError('end_date')))
+  })
+
+  test('should call UpdateEventUseCase once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(updateEventUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(updateEventUseCase.execute).toHaveBeenCalledWith({
+      id: 'anyId',
+      calendar_id: 'AnyCalendarId',
+      name: 'AnyEventName',
+      category: 'NORMAL',
+      start_date: new Date(),
+      end_date: new Date()
+
+    })
+  })
+
+  test('should return a new Event', async () => {
+    const response = await sut.execute(input)
+
+    expect(response).toEqual({
+      statusCode: 201,
+      body: {
+        id: 'anyId',
+        calendar_id: 'anyCalendarId',
+        category: 'NORMAL',
+        name: 'anyName',
+        start_date: new Date('2023-01-01'),
+        end_date: new Date('2023-01-03')
+      }
+    })
+  })
+
+  test('should throw if updateEventUseCase throws', async () => {
+    updateEventUseCase.execute.mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const response = await sut.execute(input)
+
+    expect(response).toEqual(serverError(new Error()))
   })
 })
