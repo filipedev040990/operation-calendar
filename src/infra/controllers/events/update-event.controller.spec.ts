cf9f35a30115e72da85c0cd8f0847ed4e6eaf0fc
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-11 18:32:46 -0300

    feat: ensure UpdateEventController call getEventByName once and with correct name

--- src/infra/controllers/events/update-event.controller.spec.ts
+++ src/infra/controllers/events/update-event.controller.spec.ts
@@ -1,10 +1,17 @@
+import { GetCalendarByIdUseCaseInterface } from '@/application/interfaces'
+import { GetEventByNameUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
-import { InvalidParamError, MissingParamError } from '@/shared/errors'
-import { badRequest } from '@/shared/helpers/http'
+import { InvalidParamError, MissingParamError, ResourceConflictError } from '@/shared/errors'
+import { badRequest, conflict } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 import MockDate from 'mockdate'
 
 export class UpdateEventController implements ControllerInterface {
+  constructor (
+    private readonly getCalendarByIdUseCase: GetCalendarByIdUseCaseInterface,
+    private readonly getEventByName: GetEventByNameUseCaseInterface
+  ) {}
+
   async execute (input: HttpRequest): Promise<HttpResponse> {
     const missingParam = this.requiredParamsValidator(input)
     if (missingParam) {
@@ -14,6 +21,16 @@ export class UpdateEventController implements ControllerInterface {
     const invalidCategory = this.categoryValidator(input.body.category)
     if (invalidCategory) {
       return badRequest(new InvalidParamError('category'))
+    }
+
+    const calendar = await this.getCalendarByIdUseCase.execute(input.body.calendar_id)
+    if (!calendar) {
+      return badRequest(new InvalidParamError('calendar_id'))
+    }
+
+    const event = await this.getEventByName.execute(input.body.name)
+    if (event) {
+      return conflict(new ResourceConflictError('This event already exists'))
     }
     return null
   }
@@ -37,13 +54,25 @@ export class UpdateEventController implements ControllerInterface {
   }
 }
 
+const getCalendarByIdUseCase: jest.Mocked<GetCalendarByIdUseCaseInterface> = {
+  execute: jest.fn().mockResolvedValue({
+    id: 'anyId',
+    name: 'any Name',
+    created_at: new Date('2023-01-01 19:56:10')
+  })
+}
+
+const getEventByName: jest.Mocked<GetEventByNameUseCaseInterface> = {
+  execute: jest.fn().mockResolvedValue(null)
+}
+
 describe('UpdateEventController', () => {
   let sut: UpdateEventController
   let input: HttpRequest
 
   beforeAll(() => {
     MockDate.set(new Date())
-    sut = new UpdateEventController()
+    sut = new UpdateEventController(getCalendarByIdUseCase, getEventByName)
   })
 
   beforeEach(() => {
@@ -90,5 +119,19 @@ describe('UpdateEventController', () => {
     const response = await sut.execute(input)
 
     expect(response).toEqual(badRequest(new InvalidParamError('category')))
+  })
+
+  test('should call GetCalendarByIdUseCase once and with correct calendar_id', async () => {
+    await sut.execute(input)
+
+    expect(getCalendarByIdUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(getCalendarByIdUseCase.execute).toHaveBeenCalledWith('AnyCalendarId')
+  })
+
+  test('should call getEventByName once and with correct name', async () => {
+    await sut.execute(input)
+
+    expect(getEventByName.execute).toHaveBeenCalledTimes(1)
+    expect(getEventByName.execute).toHaveBeenCalledWith('AnyEventName')
   })
 })
