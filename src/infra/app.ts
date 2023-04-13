e4c8f5c9fb153078ac2622dc2c8eb6b21a3fc11c
Filipe Siqueira <filipesiqueira@Filipes-iMac.local>
2023-04-13 12:04:04 -0300

    feat: add swagger

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../../swagger.json'
 
 const app = express()
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
