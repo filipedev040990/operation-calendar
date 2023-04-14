53b21de3bf892257765a9b52a8ad44c78094fb29
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-14 11:04:07 -0300

    feat: ensure GetEventByIdController call GetEventByIdUseCase once and with correct id

---
+++ src/infra/controllers/events/get-event-by-id.controller.spec.ts
@@ -0,0 +1,35 @@
+import { GetEventByIdUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
+import { ControllerInterface } from '@/infra/interfaces/controller.interface'
+import { HttpRequest, HttpResponse } from '@/shared/types/http'
+
+export class GetEventByIdController implements ControllerInterface {
+  constructor (private readonly getEventByIdUseCase: GetEventByIdUseCaseInterface) {}
+  async execute (input: HttpRequest): Promise<HttpResponse> {
+    await this.getEventByIdUseCase.execute(input.params?.id)
+    return null
+  }
+}
+
+const getEventByIdUseCase: jest.Mocked<GetEventByIdUseCaseInterface> = {
+  execute: jest.fn()
+}
+
+describe('GetEventByIdController', () => {
+  let input: HttpRequest
+  let sut: GetEventByIdController
+
+  beforeAll(() => {
+    input = {
+      params: {
+        id: 'anyEventId'
+      }
+    }
+    sut = new GetEventByIdController(getEventByIdUseCase)
+  })
+  test('should call GetEventByIdUseCase once and with correct id', async () => {
+    await sut.execute(input)
+
+    expect(getEventByIdUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(getEventByIdUseCase.execute).toHaveBeenCalledWith('anyEventId')
+  })
+})
