bda11b2955b4486b3d3efe383df3c287db954599
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-14 12:23:04 -0300

    feat: ensure DeleteEventController call DeleteEventUseCase once and with correct id

--- src/infra/controllers/events/delete-event.controller.spec.ts
+++ src/infra/controllers/events/delete-event.controller.spec.ts
@@ -2,13 +2,21 @@ import { MissingParamError } from '@/shared/errors'
 import { badRequest } from '@/shared/helpers/http'
 import { DeleteEventController } from './delete-event.controller'
 import { HttpRequest } from '@/shared/types/http'
+import { DeleteEventUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
+
+const deleteEventUseCase: jest.Mocked<DeleteEventUseCaseInterface> = {
+  execute: jest.fn()
+}
 
 describe('DeleteEventController', () => {
   let sut: DeleteEventController
   let input: HttpRequest
 
   beforeAll(() => {
-    sut = new DeleteEventController()
+    sut = new DeleteEventController(deleteEventUseCase)
+  })
+
+  beforeEach(() => {
     input = {
       params: {
         id: 'anyEventId'
@@ -21,5 +29,12 @@ describe('DeleteEventController', () => {
     const response = await sut.execute(input)
 
     expect(response).toEqual(badRequest(new MissingParamError('id')))
+  })
+
+  test('should call DeleteEventUseCase once and with correct id', async () => {
+    await sut.execute(input)
+
+    expect(deleteEventUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(deleteEventUseCase.execute).toHaveBeenCalledWith('anyEventId')
   })
 })
--- src/infra/controllers/events/delete-event.controller.ts
+++ src/infra/controllers/events/delete-event.controller.ts
@@ -1,13 +1,16 @@
+import { DeleteEventUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
 import { MissingParamError } from '@/shared/errors'
 import { badRequest } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 
 export class DeleteEventController implements ControllerInterface {
+  constructor (private readonly deleteEventUseCase: DeleteEventUseCaseInterface) {}
   async execute (input: HttpRequest): Promise<HttpResponse> {
     if (!input?.params?.id) {
       return badRequest(new MissingParamError('id'))
     }
+    await this.deleteEventUseCase.execute(input.params.id)
     return null
   }
 }
