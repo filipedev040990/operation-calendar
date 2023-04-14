cb16c3f927944dc435eaecc7277cac13f65fcc7c
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-14 11:06:56 -0300

    feat: ensure GetEventByIdController return null if GetEventByIdUseCase returns null

--- src/infra/controllers/events/get-event-by-id.controller.spec.ts
+++ src/infra/controllers/events/get-event-by-id.controller.spec.ts
@@ -1,12 +1,13 @@
 import { GetEventByIdUseCaseInterface } from '@/application/interfaces/event-usecase.interface'
 import { ControllerInterface } from '@/infra/interfaces/controller.interface'
+import { noContent } from '@/shared/helpers/http'
 import { HttpRequest, HttpResponse } from '@/shared/types/http'
 
 export class GetEventByIdController implements ControllerInterface {
   constructor (private readonly getEventByIdUseCase: GetEventByIdUseCaseInterface) {}
   async execute (input: HttpRequest): Promise<HttpResponse> {
     await this.getEventByIdUseCase.execute(input.params?.id)
-    return null
+    return noContent()
   }
 }
 
@@ -31,5 +32,11 @@ describe('GetEventByIdController', () => {
 
     expect(getEventByIdUseCase.execute).toHaveBeenCalledTimes(1)
     expect(getEventByIdUseCase.execute).toHaveBeenCalledWith('anyEventId')
+  })
+
+  test('should return null if GetEventByIdUseCase returns null', async () => {
+    const response = await sut.execute(input)
+
+    expect(response).toEqual(noContent())
   })
 })
