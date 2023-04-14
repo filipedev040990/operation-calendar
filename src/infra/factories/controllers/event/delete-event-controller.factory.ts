9615159370157b082888426250e004c7542941f4
Filipe Siqueira <filipedev040990@gmail.com>
2023-04-14 13:35:27 -0300

    refactor: update delete event factory

--- src/infra/factories/controllers/event/delete-event-controller.factory.ts
+++ src/infra/factories/controllers/event/delete-event-controller.factory.ts
@@ -1,7 +1,9 @@
 import { DeleteEventController } from '@/infra/controllers/events/delete-event.controller'
 import { makeDeleteEventUseCaseFactory } from '../../usecases/event/delete-event-usecase.factory'
+import { makeGetEventByIdUseCaseFactory } from '../../usecases/event/get-event-by-id-usecase.factory'
 
 export const makeDeleteEventControllerFactory = (): DeleteEventController => {
+  const getEventById = makeGetEventByIdUseCaseFactory()
   const deleteEventUseCase = makeDeleteEventUseCaseFactory()
-  return new DeleteEventController(deleteEventUseCase)
+  return new DeleteEventController(getEventById, deleteEventUseCase)
 }
